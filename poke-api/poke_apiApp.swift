a405795dace1128acc1f905ed94e4f1f640f2c03
Álvaro Fuentenebro <alvarofuentenebrogomez@gmail.com>
2023-03-14 18:15:12 +0100

    Initial Commit

---
+++ poke-api/poke_apiApp.swift
@@ -0,0 +1,17 @@
+//
+//  poke_apiApp.swift
+//  poke-api
+//
+//  Created by Alvaro Fuentenebro on 14/3/23.
+//
+
+import SwiftUI
+
+@main
+struct poke_apiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
